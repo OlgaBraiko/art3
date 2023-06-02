821968b2387bbee1526c5f02684b3d1e6537b37e
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-06-02 10:00:44 -0700

    Create mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,25 @@
- 
 import modals from "../../ts/modal";
 
 import slides from "../../ts/sliders";
- 
- 
+
 import modals from "../../ts/modal";
 
- 
- 
- 
- 
- 
+import mask from "../../ts/mask";
 
 import form from "../../ts/forms";
 
 window.addEventListener("DOMContentLoaded", () => {
- 
   modals();
- 
-   modals();
- 
+
+  modals();
+
   slides(
     ".feedback-slider-item",
     "horizontal",
     ".main-prev-btn",
     ".main-next-btn"
   );
- 
+
   form();
- 
- 
+  mask('[name="phone"]');
 });
