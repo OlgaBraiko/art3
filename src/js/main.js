bbf9d6c3f2bd9aabd0e5a3445e1f2ffc92374bbb
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-06-03 01:40:52 -0700

    show cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,23 @@
- 
 import modals from "../../ts/modal";
 
 import slides from "../../ts/sliders";
- 
- 
-import modals from "../../ts/modal";
-
- 
- 
- 
- 
- 
 
 import form from "../../ts/forms";
 
+import showMoreStyles from "../../ts/showMoreStyles";
+
 window.addEventListener("DOMContentLoaded", () => {
- 
   modals();
- 
-   modals();
- 
+
+  modals();
+
   slides(
     ".feedback-slider-item",
     "horizontal",
     ".main-prev-btn",
     ".main-next-btn"
   );
- 
+
   form();
- 
- 
+  showMoreStyles(".button-styles", ".styles-2");
 });
